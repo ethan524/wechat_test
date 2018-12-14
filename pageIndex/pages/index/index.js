c3608825f3faac48a37b16438e040b3e1f2b0c95
ethan524 <471323829@qq.com>
2018-12-14 18:33:38 +0800

    分包

---
+++ pageIndex/pages/index/index.js
@@ -0,0 +1,16 @@
+//index.js
+//获取应用实例
+const app = getApp()
+Page({
+  data: {
+    "background" : "/images/mipmap-hdpi/backgr.png",  //背景图片
+    "logo_robot": "../../../images/mipmap-hdpi/logo-robot.png",
+  },
+  onLoad: function () {
+    var that = this;
+    let base64 = wx.getFileSystemManager().readFileSync(this.data.background, 'base64');
+    that.setData({
+      'background': 'data:image/jpg;base64,' + base64
+    });
+  },
+})
