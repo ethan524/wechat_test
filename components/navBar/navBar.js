1f8d2f394b8fce0e6bbf3e271e4e1af93786d68d
ethan524 <471323829@qq.com>
2018-12-18 11:03:02 +0800

    添加了返回

--- components/navBar/navBar.js
+++ components/navBar/navBar.js
@@ -1,4 +1,25 @@
 const app = getApp();
 Component({
-
+  data : {
+    "back_style" : 'none'
+  },
+  ready : function(){
+    let that = this;
+    let page_url = getCurrentPages()[0].route;
+    let page_arr = page_url.split('/');
+    if(page_arr[1] != 'index' && page_arr[2] != 'index'){
+      console.log(page_url);
+      that.setData({
+        "back_style" : "block"
+      });
+    }
+  },
+  methods : {
+    goBack(){
+      console.log('go back');
+      wx.redirectTo({
+        "url": "/pages/index/index"
+      })
+    }
+  }
 })
