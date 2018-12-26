1290a94b22542c095f1155ad2b350f3bb43cc5f9
ethan524 <471323829@qq.com>
2018-12-26 18:17:40 +0800

    新建机器人

---
+++ pages/createRobot/createRobot.js
@@ -0,0 +1,37 @@
+const app = getApp();
+
+Page({
+  
+  data : {
+    "detail" : false
+  },
+
+  onLoad : function(){
+    wx.setNavigationBarTitle({
+      title: "新建机器人"
+    })
+    var that = this;
+    var phone = wx.getSystemInfoSync()
+    var model = phone.model;
+    if (model.indexOf("iPhone") != -1){
+      that.setData({
+        "detail": true
+      });
+    }else{
+      that.setData({
+        "detail": false
+      });
+    }
+    // if(phone.platform == "ios"){
+    //   that.setData({
+    //     "detail": true
+    //   });
+    // }else if(phone.platform == "android"){
+    //   that.setData({
+    //     "detail": false
+    //   });
+    // }
+
+  }
+
+})
